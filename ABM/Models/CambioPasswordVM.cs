8f8a61a0f8578596a06c2f0c5b77f416e7c38dff
ivancitoflow11 <flowoverride@gmail.com>
2025-07-21 11:08:57 -0400

    REVISION DE QUE LOS CAMBIOS DE CONTRASEÑA FUNCIONASEN

--- ABM/Models/CambioPasswordVM.cs
+++ ABM/Models/CambioPasswordVM.cs
@@ -8,8 +8,8 @@ namespace ABM.Models
 
         [Required(ErrorMessage = "La nueva contraseña es obligatoria.")]
         [StringLength(100, ErrorMessage = "La contraseña debe tener al menos 14 caracteres.", MinimumLength = 14)]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d\._\-!@#$%^&()+=]{14,}$",
-    ErrorMessage = "La contraseña debe contener mínimo 14 caracteres, al menos una letra mayúscula y un número.")]
+        [RegularExpression(@"^(?=.*[a-zñ])(?=.*[A-ZÑ])(?=.*\d)[A-Za-zñÑ\d\._\-!@#$%^&()+=]{14,}$",
+            ErrorMessage = "La contraseña debe tener mínimo 14 caracteres, al menos una mayúscula, una minúscula  y un número.")]
         public string NuevaContrasena { get; set; }
 
 
